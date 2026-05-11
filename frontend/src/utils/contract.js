790c2d7653bde3b0d429196ae56c5daedd0faabf
Krrish <krrish4173@gmail.com>
2026-05-11 10:48:10 +0530

    fix: force sync marketplace ABI for cancelListing

--- frontend/src/utils/contract.js
+++ frontend/src/utils/contract.js
@@ -1,4 +1,25 @@
 import NFTMarketplaceArtifact from './NFTMarketplace.json';
 
 export const MARKETPLACE_ADDRESS = import.meta.env.VITE_MARKETPLACE_ADDRESS;
-export const NFTMarketplaceABI = NFTMarketplaceArtifact.abi;
+
+// Force-hardcoding the critical functions to prevent JSON sync issues
+export const NFTMarketplaceABI = [
+  ...NFTMarketplaceArtifact.abi.filter(item => item.name !== 'cancelListing' && item.name !== 'resellToken'),
+  {
+    "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "cancelListing",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "uint256", "name": "tokenId", "type": "uint256" },
+      { "internalType": "uint256", "name": "price", "type": "uint256" }
+    ],
+    "name": "resellToken",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  }
+];
