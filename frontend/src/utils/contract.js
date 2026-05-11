bb7377c81b3b198f009eac467ceae3630ec6427b
Krrish <krrish4173@gmail.com>
2026-05-11 10:55:57 +0530

    chore: redeploy marketplace contract and sync fresh ABI

--- frontend/src/utils/contract.js
+++ frontend/src/utils/contract.js
@@ -1,25 +1,4 @@
 import NFTMarketplaceArtifact from './NFTMarketplace.json';
 
 export const MARKETPLACE_ADDRESS = import.meta.env.VITE_MARKETPLACE_ADDRESS;
-
-// Force-hardcoding the critical functions to prevent JSON sync issues
-export const NFTMarketplaceABI = [
-  ...NFTMarketplaceArtifact.abi.filter(item => item.name !== 'cancelListing' && item.name !== 'resellToken'),
-  {
-    "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
-    "name": "cancelListing",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "uint256", "name": "tokenId", "type": "uint256" },
-      { "internalType": "uint256", "name": "price", "type": "uint256" }
-    ],
-    "name": "resellToken",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
-  }
-];
+export const NFTMarketplaceABI = NFTMarketplaceArtifact.abi;
