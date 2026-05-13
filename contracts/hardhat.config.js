3d92606d41b2b59b389ce8173450bed90502b698
Krrish <krrish4173@gmail.com>
2026-05-13 11:36:44 +0530

    feat: add etherscan config for SCAI verification

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -17,5 +17,20 @@ export default {
       url: process.env.SCAI_RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     }
-  }
+  },
+  etherscan: {
+    apiKey: {
+      scai: "ANY_STRING",
+    },
+    customChains: [
+      {
+        network: "scai",
+        chainId: 34,
+        urls: {
+          apiURL: "https://explorer.securechain.ai/api",
+          browserURL: "https://explorer.securechain.ai",
+        },
+      },
+    ],
+  },
 };
